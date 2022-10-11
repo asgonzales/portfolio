1809b3c0a4d8dbc8397aa0b592315e2cc28b09b1
asgonzales <asgonzalesromani@gmail.com>
2022-10-11 13:43:42 -0300

    Update info

--- src/components/Cards/HomeCard/HomeCard.jsx
+++ src/components/Cards/HomeCard/HomeCard.jsx
@@ -17,7 +17,7 @@ export default function HomeCard () {
             <h3>Hello there! My name is Sebastian Gonzales</h3>
             <p>and I'm a Full Stack Developer from Argentina.</p>
             <p>Welcome to my portfolio!</p>
-            <span>07/10/2022: Working on a new versión of this portfolio {'>:)'}</span>
+            <span>11/10/2022: Still working on a new versión of this portfolio {'>:D'}</span>
         </div>
     )
 }
