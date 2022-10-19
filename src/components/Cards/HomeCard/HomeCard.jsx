9ae280acbdacf457945edc8ade804afabf704a47
asgonzales <asgonzalesromani@gmail.com>
2022-10-19 20:55:22 -0300

    Update JSON

--- src/components/Cards/HomeCard/HomeCard.jsx
+++ src/components/Cards/HomeCard/HomeCard.jsx
@@ -9,7 +9,9 @@ import style from './HomeCard.module.css';
 
 export default function HomeCard () {
 
-
+    const openPortfolio = () => {
+        window.open('https://asgonzales-3d-portfolio.vercel.app/')
+    }
 
 
     return (
@@ -17,7 +19,8 @@ export default function HomeCard () {
             <h3>Hello there! My name is Sebastian Gonzales</h3>
             <p>and I'm a Full Stack Developer from Argentina.</p>
             <p>Welcome to my portfolio!</p>
-            <span>15/10/2022: Almost done! :D {'>:D'}</span>
+            {/* <span>15/10/2022: Almost done! :D {'>:D'}</span> */}
+            <button className={style.button} onClick={openPortfolio}>Check the 3D version</button>
         </div>
     )
 }
