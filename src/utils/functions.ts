c964af31d478186f059f360b2e45066e1adab042
asgonzales <asgonzalesromani@gmail.com>
2023-04-24 17:07:35 -0300

    fix: NodeJs error

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -22,7 +22,7 @@ export function debounce<Params extends any[]>(
   func: (...args: Params) => any,
   timeout: number,
 ): (...args: Params) => void {
-  let timer: NodeJs.Timeout
+  let timer: NodeJS.Timeout
   return (...args: Params) => {
     clearTimeout(timer)
     timer = setTimeout(() => {
